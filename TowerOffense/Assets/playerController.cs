4c95210ae04aba585ec73bb55fda69a94afea44f
Ben DeYoung <deyo0039@gmail.com>
2014-05-22 12:42:28 -0500

    player now stops when he stops

--- TowerOffense/Assets/playerController.cs
+++ TowerOffense/Assets/playerController.cs
@@ -17,20 +17,26 @@ public class playerController : MonoBehaviour {
 		if (vertical > 0)
 		{
 			animator.SetInteger("Direction", 2);
-			if(Input.GetKeyUp("up")){
-			}
+			animator.SetFloat("Change", 1.0f);
 		}
 		else if (vertical < 0)
 		{
 			animator.SetInteger("Direction", 0);
+			animator.SetFloat("Change", 1.0f);
 		}
 		else if (horizontal > 0)
 		{
 			animator.SetInteger("Direction", 3);
+			animator.SetFloat("Change", 1.0f);
 		}
 		else if (horizontal < 0)
 		{
 			animator.SetInteger("Direction", 1);
+			animator.SetFloat("Change", 1.0f);
+		}
+		else
+		{
+			animator.SetFloat("Change", 0.0f);
 		}
 	}
 }
