5c5c7cd383c9fb528dee1e1cb17be9af587be3ba
Ben DeYoung <deyo0039@gmail.com>
2014-05-28 16:30:51 -0500

    arrows are now in 2d instead of 3d

---
+++ TowerOffense/Assets/mover2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover2 : MonoBehaviour {
+
+	public float speed = 3.0f;
+	// Update is called once per frame
+	void Start () {
+		rigidbody2D.velocity = transform.up * speed;
+	}
+}
