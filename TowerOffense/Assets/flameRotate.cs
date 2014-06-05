945973922d5a6c53bfdc651e3f96f65f33b54c23
Paul <paul.e.lanctot@gmail.com>
2014-06-05 15:48:04 -0500

    Finished tower sprite and flame animation

---
+++ TowerOffense/Assets/flameRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flameRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		// Slowly rotate the object around its X axis at 20 degree/second.
+		//transform.Rotate(Vector3.right, 20 * Time.deltaTime);
+		// ... at the same time as spinning it relative to the global 
+		// Y axis at the same speed.
+		transform.Rotate(Vector3.back, 108 * Time.deltaTime, Space.World);
+	}
+}
