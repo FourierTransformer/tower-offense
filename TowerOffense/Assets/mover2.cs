9efbb9a058c78c64b19dfa57892c3c428dd5bfab
Ben DeYoung <deyo0039@gmail.com>
2014-06-06 11:12:02 -0500

    arrows are now destroyed when hitting npcs

--- TowerOffense/Assets/mover2.cs
+++ TowerOffense/Assets/mover2.cs
@@ -5,6 +5,14 @@ public class mover2 : MonoBehaviour {
 
 	public float speed = 8.0f;
 
+	void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.name == "meleemonster" || collision.gameObject.name == "archer" ||
+		    collision.gameObject.name == "grenadier" || collision.gameObject.name == "landminedropper" || 
+		    collision.gameObject.name == "suicidebomber" || collision.gameObject.name == "wallhugger") {
+			Destroy(gameObject);
+		}
+	}
+
 	void Update(){
 		rigidbody2D.AddForce (transform.up * speed);
 	}
