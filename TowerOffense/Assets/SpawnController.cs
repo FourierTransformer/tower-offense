477e6a516a114de1c45dfeb6b47effbf7a7e6018
Paul <paul.e.lanctot@gmail.com>
2014-06-25 16:47:34 -0500

    small changes, need to get health modifier to work

--- TowerOffense/Assets/SpawnController.cs
+++ TowerOffense/Assets/SpawnController.cs
@@ -18,7 +18,8 @@ public class SpawnController : MonoBehaviour
 	public float HealthModifier;
 	public float SpeedModifier;
 	public float levelWait;
-	
+
+	/* get health to modify, don't spawn on lake, spawn all enemies without multiple for loops? */
 	void Start ()
 	{
 		StartCoroutine (SpawnWaves ());
@@ -29,6 +30,16 @@ public class SpawnController : MonoBehaviour
 		//yield return new WaitForSeconds (startWait);
 		while (true)
 		{
+			/*
+			if (HealthModifier > 0)
+			{
+				//enemy.health = HealthModifier * enemy.health;
+			}
+			if (SpeedModifier > 0)
+			{
+				enemy.speed = HealthModifier * enemy.speed;
+			}
+			*/
 			for (int i = 0; i < enemyCount; i++)
 			{
 				//Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
